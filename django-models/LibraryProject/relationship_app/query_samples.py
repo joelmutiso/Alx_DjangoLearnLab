e7b61df7a228c1536b8e9370a3a7ddae2b2d2226
Joel Mutiso <joelwmutiso@gmail.com>
2025-08-17 00:10:07 +0300

    adding query samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,42 +1,25 @@
-"""
-Sample queries demonstrating relationships between Author, Book, Library, and Librarian.
-Run this script using:
-    python manage.py shell < relationship_app/query_samples.py
-"""
+import django
+import os
 
-from relationship_app.models import Author, Library
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
+django.setup()
 
-# 1. Query all books by a specific author
+from relationship_app.models import Author, Book, Library, Librarian
+
 author_name = "J.K. Rowling"
-try:
-    author = Author.objects.get(name=author_name)
-    books_by_author = author.books.all()
-    print(f"Books by {author_name}:")
-    for book in books_by_author:
-        print(f"- {book.title}")
-except Author.DoesNotExist:
-    print(f"No author found with name '{author_name}'.")
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+print(f"Books by {author_name}:")
+for book in books_by_author:
+    print(book.title)
 
-print("\n" + "-"*50 + "\n")
-
-# 2. List all books in a library
 library_name = "Central Library"
-try:
-    library = Library.objects.get(name=library_name)
-    books_in_library = library.books.all()
-    print(f"Books in {library_name}:")
-    for book in books_in_library:
-        print(f"- {book.title} (by {book.author.name})")
-except Library.DoesNotExist:
-    print(f"No library found with name '{library_name}'.")
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+print(f"\nBooks in {library_name}:")
+for book in books_in_library:
+    print(book.title)
 
-print("\n" + "-"*50 + "\n")
-
-# 3. Retrieve the librarian for a library
-try:
-    librarian = library.librarian
-    print(f"The librarian at {library_name} is {librarian.name}.")
-except Library.DoesNotExist:
-    print(f"No library found with name '{library_name}'.")
-except AttributeError:
-    print(f"No librarian assigned to '{library_name}'.")
+library_for_librarian = Library.objects.get(name=library_name)
+librarian = Librarian.objects.get(library=library_for_librarian)
+print(f"\nLibrarian for {library_name}: {librarian.name}")
