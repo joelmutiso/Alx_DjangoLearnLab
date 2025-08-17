e7656e4618a7780a48c8c547e38f4981cd5da7de
Joel Mutiso <joelwmutiso@gmail.com>
2025-08-17 20:03:16 +0300

    add authentication redirect settings

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 from .views import CustomLoginView, CustomLogoutView, register_view
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -8,5 +10,8 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('register/', register_view, name='register'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
 
