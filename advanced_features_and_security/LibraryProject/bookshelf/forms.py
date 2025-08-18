b4d9c1393447f95d540ee08a00c2cf3a237bb0f4
Joel Mutiso <joelwmutiso@gmail.com>
2025-08-18 17:04:24 +0300

    Add security settings to settings.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,14 +1,6 @@
 from django import forms
 
-class SearchForm(forms.Form):
-    query = forms.CharField(
-        max_length=100,
-        required=False,
-        strip=True,
-        widget=forms.TextInput(attrs={"placeholder": "Search books..."})
-    )
-
-    def clean_query(self):
-        q = self.cleaned_data.get("query", "")
-        
-        return q
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=100, required=True)
+    author = forms.CharField(max_length=100, required=True)
+    published_date = forms.DateField(required=True, widget=forms.DateInput(attrs={'type': 'date'}))
