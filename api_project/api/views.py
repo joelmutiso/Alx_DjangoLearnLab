318845c60ab50d944c9b9777eb87c50e43329d48
Joel Mutiso <joelwmutiso@gmail.com>
2025-08-19 02:05:52 +0300

    Enforce authentication and permissions on API endpoints

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from .models import Book 
 from .serializers import BookSerializer
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
