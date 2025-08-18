cdea88295ab8352433923705ad91b34c4a198034
Joel Mutiso <joelwmutiso@gmail.com>
2025-08-19 02:03:45 +0300

    Enforce authentication and permissions on API endpoints

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Include the API URLs
+    path('api-token-auth/', views.obtain_auth_token),  # Token authentication endpoint
 ]
