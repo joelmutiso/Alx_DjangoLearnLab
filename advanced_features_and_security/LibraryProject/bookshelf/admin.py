ad21723e64939182d249dac95e9a572c9141daf7
Joel Mutiso <joelwmutiso@gmail.com>
2025-08-18 16:02:35 +0300

    Add CustomUserManager with create_user and create_superuser methods

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .models import Book
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # fields to show in list view
-    search_fields = ('title', 'author')  # adds a search box
-    list_filter = ('publication_year',)  # adds a filter sidebar
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
 
-# Register the Book model with this custom admin
-admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
