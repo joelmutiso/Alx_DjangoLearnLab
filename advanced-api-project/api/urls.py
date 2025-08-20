eb0767b520700f0bed417f5a060ac3ab3d815d11
Joel Mutiso <joelwmutiso@gmail.com>
2025-08-20 21:19:40 +0300

    Corrected URL patterns and added migration

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,9 +2,12 @@ from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView,
 from django.urls import path
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),
+    # Paths for a specific book should come before the base URL
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
     path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
+    
+    # Path for the list view comes last as it's the most general
+    path('books/', BookListView.as_view(), name='book-list'),
 ]
