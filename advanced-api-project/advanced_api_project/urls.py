5885802b849c20994ba8cb6b31749ac83d5eae0c
Joel Mutiso <joelwmutiso@gmail.com>
2025-08-20 16:30:17 +0300

    Implement generic Django views and URL patterns

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -1,22 +1,16 @@
-"""
-URL configuration for advanced_api_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Path for retrieving all books
+    path('books/', BookListView.as_view(), name='book-list'),
+
+    # Path for creating a new book
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+
+    # Path for retrieving, updating, or deleting a specific book
+    # The <int:pk> captures the book's primary key (ID)
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Book
 
-# Create your views here.
+class BookListView(ListView):
+    """
+    A view to retrieve a list of all books.
+    """
+    model = Book
+    queryset = Book.objects.all()
+
+class BookDetailView(DetailView):
+    """
+    A view to retrieve a single book by its ID.
+    """
+    model = Book
+
+class BookCreateView(CreateView):
+    """
+    A view to create a new book.
+    """
+    model = Book
+    fields = ['title', 'publication_year', 'author']
+
+class BookUpdateView(UpdateView):
+    """
+    A view to update an existing book.
+    """
+    model = Book
+    fields = ['title', 'publication_year', 'author']
+
+class BookDeleteView(DeleteView):
+    """
+    A view to delete a book.
+    """
+    model = Book
+    success_url = '/books/'
