0464b0456eafe7a4a243e09a255334f92056edf5
Joel Mutiso <joelwmutiso@gmail.com>
2025-08-21 20:43:18 +0300

    Add CRUD operations Comments

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
 
     # Comment URLs (now class-based)
     path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
-    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='edit-comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='update-comment'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
 
     # User authentication paths
@@ -32,3 +32,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
 ]
+
